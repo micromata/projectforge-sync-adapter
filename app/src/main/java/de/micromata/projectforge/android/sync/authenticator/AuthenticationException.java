db702fd40128cd1c311f3c60f35074941d528244
Lado Kumsiashvili <herrlado@gmail.com>
2019-10-30 15:08:00 +0100

    realse 3.0

---
+++ app/src/main/java/de/micromata/projectforge/android/sync/authenticator/AuthenticationException.java
@@ -0,0 +1,8 @@
+package de.micromata.projectforge.android.sync.authenticator;
+
+public class AuthenticationException extends IllegalStateException
+{
+  public AuthenticationException(String message){
+    super(message);
+  }
+}
---
+++ app/src/main/java/de/micromata/projectforge/android/sync/client/ParseException.java
@@ -0,0 +1,9 @@
+package de.micromata.projectforge.android.sync.client;
+
+public class ParseException extends RuntimeException
+{
+  public ParseException(String message)
+  {
+    super(message);
+  }
+}
